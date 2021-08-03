e5b1f7ee4e51dadcd0fb84044e3fd729599f5fec
ivandev <smartpagedevelop@gmail.com>
2021-08-03 09:08:48 +0300

    add pawn move

--- src/components/ChessDesk.vue
+++ src/components/ChessDesk.vue
@@ -8,6 +8,9 @@ const BISHOP = 4; // Слон
 const KNIGHT = 5; // Конь
 const PAWN = 6; // Пешка
 
+const WHITE_TEAM = 0;
+const BLACK_TEAM = 1;
+
 const FIGURE_LABELS = {
     [0]: {
         [KING]: '♔',
@@ -47,28 +50,103 @@ const generateStartChessDesk = (): ChessDeskMatrix => {
         const row: Array<ChessFigure> = [];
         for (let j=0; j<8; ++j){
             switch (i){
-                case 0: row.push(createChessFigure(1, PAWN)); break;
-                case 5: row.push(createChessFigure(0, PAWN)); break;
+                case 0: row.push(createChessFigure(BLACK_TEAM, PAWN)); break;
+                case 5: row.push(createChessFigure(WHITE_TEAM, PAWN)); break;
                 default: row.push(0);
             }
         }
         desk.push(row);
     }
     return [
-        INITIAL_BASE_ROW.map((type) => createChessFigure(1, type)),
+        INITIAL_BASE_ROW.map((type) => createChessFigure(BLACK_TEAM, type)),
         ...desk,
-        INITIAL_BASE_ROW.map((type) => createChessFigure(0, type)),
+        INITIAL_BASE_ROW.map((type) => createChessFigure(WHITE_TEAM, type)),
     ];
+}
+
+const pawnDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Array<number[]> => {
+    let directions = [];
+    const yDir = team === WHITE_TEAM ? -1 : 1;
+    const forwardMoveY = y + yDir;
+    const forwardMoveCheck = forwardMoveY !== 8 && forwardMoveY !== -1
+    if (forwardMoveCheck && matrix[forwardMoveY][x] === 0){
+        directions.push([x, forwardMoveY]);
+
+        if (team === WHITE_TEAM && y === 6 && matrix[y-2][x] === 0){
+            directions.push([x, y-2]);
+        } else if (team === BLACK_TEAM && y === 1 && matrix[y+2][x] === 0){
+            directions.push([x, y+2]);
+        }
+    }
+    if (x > 0 && forwardMoveCheck){
+        const enemy = matrix[forwardMoveY][x-1];
+        if (enemy !== 0 && enemy.team !== team){
+            directions.push([x-1, forwardMoveY]);
+        }
+    }
+    if (x < 7 && forwardMoveCheck){
+        const enemy = matrix[forwardMoveY][x+1];
+        if (enemy !== 0 && enemy.team !== team){
+            directions.push([x+1, forwardMoveY]);
+        }
+    }
+
+    return directions;
 }
 
 export default defineComponent({
     name: 'ChessDesk',
     setup(){
         const chessDeskMatrix = ref<ChessDeskMatrix>(generateStartChessDesk());
+        let teamStep = WHITE_TEAM;
+        let clickedPosition = ref<false|Array<number>>(false);
+        let possibleDirections = ref<Array<number[]>>([]);
+
+        const setPosition = (x: number, y: number) => {
+            clickedPosition.value = false;
+            possibleDirections.value = [];
+            const figure = chessDeskMatrix.value[y][x];
+            if (figure !== 0 && figure.team === teamStep){
+                clickedPosition.value = [x, y];
+                switch (figure.type){
+                    case PAWN: possibleDirections.value = pawnDirections(teamStep, chessDeskMatrix.value, x, y); break;
+                }
+            }
+        }
+
+        const move = (x: number, y: number) => {
+            if (clickedPosition.value){
+                const figure = chessDeskMatrix.value[clickedPosition.value[1]][clickedPosition.value[0]];
+                chessDeskMatrix.value[clickedPosition.value[1]][clickedPosition.value[0]] = 0;
+                chessDeskMatrix.value[y][x] = figure;
+
+                teamStep = teamStep === WHITE_TEAM ? BLACK_TEAM : WHITE_TEAM;
+                clickedPosition.value = false;
+                possibleDirections.value = [];
+            }
+        }
+
+        const clickBy = (x: number, y: number) => {
+            if (!clickedPosition.value){
+                setPosition(x, y);
+            } else {
+                const checkMove = possibleDirections.value.some((dir) => x === dir[0] && y === dir[1]);
+                if (checkMove){
+                    move(x, y);
+                } else {
+                    setPosition(x, y);
+                }
+            }
+        };
 
         return {
+            clickBy,
             chessDeskMatrix,
             FIGURE_LABELS,
+            WHITE_TEAM,
+            BLACK_TEAM,
+            possibleDirections,
+            clickedPosition,
         };
     },
 });
@@ -89,11 +167,18 @@ export default defineComponent({
                     'paired': (rowIndex + colIndex) % 2 === 1,
                     'unpaired': (rowIndex + colIndex) % 2 === 0,
                 }"
+                @click="clickBy(colIndex, rowIndex)"
             >
+                <div
+                    v-if="possibleDirections.some((dir) => colIndex === dir[0] && rowIndex === dir[1])"
+                    class="possible-move"
+                />
                 <span
                     v-if="cell !== 0"
                     class="figure"
-                    :class="cell.team === 0 ? 'white' : 'black'"
+                    :class="{
+                        'active': clickedPosition && clickedPosition[0] === colIndex && clickedPosition[1] === rowIndex,
+                    }"
                 >
                     {{ FIGURE_LABELS[cell.team][cell.type] }}
                 </span>
@@ -111,6 +196,7 @@ export default defineComponent({
         display: flex;
     }
     .cell{
+        position: relative;
         width: 50px;
         height: 50px;
         display: flex;
@@ -124,7 +210,22 @@ export default defineComponent({
             background: white;
         }
     }
+    .possible-move{
+        position: absolute;
+        left:50%;
+        top:50%;
+        width:10px;
+        height:10px;
+        border-radius: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        background: #25ac0d;
+        opacity: 50%;
+        z-index: 2;
+    }
     .figure{
+        position: relative;
+        z-index: 1;
+        user-select: none;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -132,6 +233,10 @@ export default defineComponent({
         width:30px;
         height:30px;
         background: #dbeae7;
-        border:1px solid #b6c5c0;
+        border:2px solid #b6c5c0;
+
+        &.active{
+            border-color: #25ac0d;
+        }
     }
 </style>
