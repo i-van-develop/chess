b87af05f5f1964213899358f133bb460e0ede995
ivandev <smartpagedevelop@gmail.com>
2021-08-04 09:43:36 +0300

    add all figure moves add back & next action refactor

--- src/components/ChessDesk.vue
+++ src/components/ChessDesk.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import {defineComponent, ref} from 'vue';
+import {cloneDeep} from 'lodash';
 
 const KING = 1; // Король
 const QUEEN = 2; // Королева
@@ -34,8 +35,15 @@ type ChessFigure = 0 | {
     type: number;
     team: 0|1;
 };
+type Coords = [number, number];
 
 const INITIAL_BASE_ROW = [ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK];
+const KNIGHT_MOVES: Coords[] = [ [2,1], [2,-1], [-2,1], [-2,-1], [1,2], [-1,2], [1,-2], [-1,-2] ];
+const ROOK_MOVES: Coords[] = [ [1,0], [0,1], [-1,0], [0,-1] ];
+const BISHOP_MOVES: Coords[] = [ [1,-1], [1,1], [-1,1], [-1,-1] ];
+const KING_MOVES: Coords[] = [ ...ROOK_MOVES, ...BISHOP_MOVES ];
+const QUEEN_MOVES: Coords[] = [ ...ROOK_MOVES, ...BISHOP_MOVES ];
+
 type ChessDeskMatrix = Array<Array<ChessFigure>>;
 const createChessFigure = (team: 0|1, type: number) => {
     return {
@@ -62,10 +70,37 @@ const generateStartChessDesk = (): ChessDeskMatrix => {
         ...desk,
         INITIAL_BASE_ROW.map((type) => createChessFigure(WHITE_TEAM, type)),
     ];
+};
+
+const generateDirections = (moves: Coords[], steps: number, team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Coords[] => {
+    let directions: Coords[] = [];
+    moves.forEach(([xm, ym]) => {
+        let newX = x;
+        let newY = y;
+        for(let i = 0; i<steps; ++i){
+            newX += xm;
+            newY += ym;
+
+            if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8){
+                const position = matrix[newY][newX];
+                if (position === 0){
+                    directions.push([newX, newY]);
+                } else {
+                    if (position.team !== team){
+                        directions.push([newX, newY]);
+                    }
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+    });
+    return directions;
 }
 
-const pawnDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Array<number[]> => {
-    let directions = [];
+const pawnDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Coords[] => {
+    let directions: Coords[] = [];
     const yDir = team === WHITE_TEAM ? -1 : 1;
     const forwardMoveY = y + yDir;
     const forwardMoveCheck = forwardMoveY !== 8 && forwardMoveY !== -1
@@ -92,15 +127,37 @@ const pawnDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number
     }
 
     return directions;
-}
+};
+
+const knightDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Coords[] => {
+    return generateDirections( KNIGHT_MOVES, 1, team, matrix, x, y);
+};
+
+const rookDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Coords[] => {
+    return generateDirections(ROOK_MOVES, 7, team, matrix, x, y);
+};
+
+const bishopDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Coords[] => {
+    return generateDirections(BISHOP_MOVES, 7, team, matrix, x, y);
+};
+
+const queenDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Coords[] => {
+    return generateDirections(QUEEN_MOVES, 7, team, matrix, x, y);
+};
+
+const kingDirections = (team: 0|1, matrix: ChessDeskMatrix, x: number, y: number): Coords[] => {
+    return generateDirections(KING_MOVES, 1, team, matrix, x, y);
+};
 
 export default defineComponent({
     name: 'ChessDesk',
     setup(){
         const chessDeskMatrix = ref<ChessDeskMatrix>(generateStartChessDesk());
+        const backBuffer = ref<ChessDeskMatrix[]>([]);
+        const nextBuffer = ref<ChessDeskMatrix[]>([]);
         let teamStep = WHITE_TEAM;
-        let clickedPosition = ref<false|Array<number>>(false);
-        let possibleDirections = ref<Array<number[]>>([]);
+        let clickedPosition = ref<false|Coords>(false);
+        let possibleDirections = ref<Coords[]>([]);
 
         const setPosition = (x: number, y: number) => {
             clickedPosition.value = false;
@@ -108,23 +165,53 @@ export default defineComponent({
             const figure = chessDeskMatrix.value[y][x];
             if (figure !== 0 && figure.team === teamStep){
                 clickedPosition.value = [x, y];
+                let calcDirection = null;
                 switch (figure.type){
-                    case PAWN: possibleDirections.value = pawnDirections(teamStep, chessDeskMatrix.value, x, y); break;
+                    case PAWN: calcDirection = pawnDirections; break;
+                    case BISHOP: calcDirection = bishopDirections; break;
+                    case ROOK: calcDirection = rookDirections; break;
+                    case KNIGHT: calcDirection = knightDirections; break;
+                    case QUEEN: calcDirection = queenDirections; break;
+                    case KING: calcDirection = kingDirections; break;
+                }
+                if (calcDirection){
+                    possibleDirections.value = calcDirection(teamStep, chessDeskMatrix.value, x, y);
                 }
             }
         }
 
         const move = (x: number, y: number) => {
             if (clickedPosition.value){
-                const figure = chessDeskMatrix.value[clickedPosition.value[1]][clickedPosition.value[0]];
-                chessDeskMatrix.value[clickedPosition.value[1]][clickedPosition.value[0]] = 0;
+                backBuffer.value.push(cloneDeep(chessDeskMatrix.value));
+                nextBuffer.value = [];
+                const [cx, cy] = clickedPosition.value;
+                const figure = chessDeskMatrix.value[cy][cx];
+                chessDeskMatrix.value[cy][cx] = 0;
                 chessDeskMatrix.value[y][x] = figure;
 
                 teamStep = teamStep === WHITE_TEAM ? BLACK_TEAM : WHITE_TEAM;
                 clickedPosition.value = false;
                 possibleDirections.value = [];
             }
-        }
+        };
+
+        const backMove = () => {
+            if (backBuffer.value.length > 0){
+                teamStep = teamStep === WHITE_TEAM ? BLACK_TEAM : WHITE_TEAM;
+                const popped = backBuffer.value.pop() as ChessDeskMatrix;
+                nextBuffer.value.push(cloneDeep(chessDeskMatrix.value));
+                chessDeskMatrix.value = cloneDeep(popped);
+            }
+        };
+
+        const nextMove = () => {
+            if (nextBuffer.value.length > 0){
+                teamStep = teamStep === WHITE_TEAM ? BLACK_TEAM : WHITE_TEAM;
+                const popped = nextBuffer.value.pop() as ChessDeskMatrix;
+                backBuffer.value.push(cloneDeep(chessDeskMatrix.value));
+                chessDeskMatrix.value = cloneDeep(popped);
+            }
+        };
 
         const clickBy = (x: number, y: number) => {
             if (!clickedPosition.value){
@@ -142,6 +229,10 @@ export default defineComponent({
         return {
             clickBy,
             chessDeskMatrix,
+            backBuffer,
+            nextBuffer,
+            backMove,
+            nextMove,
             FIGURE_LABELS,
             WHITE_TEAM,
             BLACK_TEAM,
@@ -153,41 +244,77 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="desk">
-        <div
-            v-for="(row, rowIndex) in chessDeskMatrix"
-            :key="rowIndex"
-            class="row"
-        >
+    <div class="desk-wrap">
+        <div class="controls">
+            <button :disabled="backBuffer.length === 0" @click="backMove">Back</button>
+            <button :disabled="nextBuffer.length === 0" @click="nextMove">Next</button>
+        </div>
+        <div class="desk">
             <div
-                v-for="(cell, colIndex) in row"
-                :key="colIndex"
-                class="cell"
-                :class="{
+                v-for="(row, rowIndex) in chessDeskMatrix"
+                :key="rowIndex"
+                class="row"
+            >
+                <div
+                    v-for="(cell, colIndex) in row"
+                    :key="colIndex"
+                    class="cell"
+                    :class="{
                     'paired': (rowIndex + colIndex) % 2 === 1,
                     'unpaired': (rowIndex + colIndex) % 2 === 0,
                 }"
-                @click="clickBy(colIndex, rowIndex)"
-            >
-                <div
-                    v-if="possibleDirections.some((dir) => colIndex === dir[0] && rowIndex === dir[1])"
-                    class="possible-move"
-                />
-                <span
-                    v-if="cell !== 0"
-                    class="figure"
-                    :class="{
+                    @click="clickBy(colIndex, rowIndex)"
+                >
+                    <div
+                        v-if="possibleDirections.some((dir) => colIndex === dir[0] && rowIndex === dir[1])"
+                        class="possible-move"
+                    />
+                    <span
+                        v-if="cell !== 0"
+                        class="figure"
+                        :class="{
                         'active': clickedPosition && clickedPosition[0] === colIndex && clickedPosition[1] === rowIndex,
                     }"
-                >
+                    >
                     {{ FIGURE_LABELS[cell.team][cell.type] }}
                 </span>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+    .desk-wrap{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .controls{
+        display: flex;
+        margin-bottom: 15px;
+
+        button{
+            margin-right: 10px;
+            background: #3737e7;
+            color:white;
+            border:0;
+            padding: 4px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+
+            &:hover{
+                background: #2c2cb6;
+            }
+            &:disabled{
+                background: #55709c;
+                color: #a6b4cb;
+                cursor: default;
+            }
+        }
+    }
+
     .desk{
         display: flex;
         flex-direction: column;
